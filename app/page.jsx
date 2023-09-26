eb41c689900a2b34c21511b982a7b5ea569f6e04
juleni <69912179+juleni@users.noreply.github.com>
2023-09-26 14:30:09 +0200

    Added home page

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Ideas Collection</span>
+      </h1>
+      <p className="desc text-center">
+        Ideas Collection is an open-source tool for collecting and sharing
+        ideas.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
