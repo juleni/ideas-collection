10a6de8c9303e7f70290f2f60708387b8ba88b90
juleni <69912179+juleni@users.noreply.github.com>
2023-09-28 14:01:18 +0200

    Added Create Idea page

---
+++ app/create-idea/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Form from "@components/Form";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const CreateIdea = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    idea: "",
+    tag: "",
+  });
+
+  const createIdea = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch("/api/idea/new", {
+        method: "POST",
+        body: JSON.stringify({
+          idea: post.idea,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("Create Idea error: " + error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createIdea}
+    />
+  );
+};
+
+export default CreateIdea;
