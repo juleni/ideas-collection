043581840b4aa21e63a3136a6a00834a70632d3f
juleni <69912179+juleni@users.noreply.github.com>
2023-09-29 17:10:44 +0200

    Implemented Edit and Delete Post

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 import Profile from "@components/Profile";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ProfilePage = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
@@ -11,14 +13,27 @@ const ProfilePage = () => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
-      console.log(data);
       setPosts(data);
     };
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-idea?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this post?");
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/idea/${post._id.toString()}`, { method: "DELETE" });
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log("Delete Post error: " + error);
+      }
+    }
+  };
 
   return (
     <Profile
