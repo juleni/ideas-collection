df317db40e18ee7c93c4531a95709e7699fd9bd5
juleni <69912179+juleni@users.noreply.github.com>
2023-09-29 14:47:12 +0200

    Added ProfilePage

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,34 @@
 "use client";
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const ProfilePage = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      console.log(data);
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name={session?.user.name || "Unknown"}
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default ProfilePage;
